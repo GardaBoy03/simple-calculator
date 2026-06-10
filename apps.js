8d2a8912a05bb1824caec2143724b919ed8c77ba
GardaBoy03 <fmuhammadgarda@gmail.com>
2026-06-10 07:33:57 +0700

    Update apps.js

--- apps.js
+++ apps.js
@@ -4,11 +4,11 @@ var app = new Vue({
         bill: '',
         bil2: '',
         operasi: '+', 
-        hasilKalkulasi: 0 
+        hasilKalkulasi: 0,
+        riwayat: [] // Menyimpan daftar riwayat kalkulasi
     },
     methods: {
         hitung() {
-            // Jika salah satu kolom input masih kosong, set hasil ke 0
             if (this.bill === '' || this.bil2 === '') {
                 this.hasilKalkulasi = 0;
                 return;
@@ -16,24 +16,37 @@ var app = new Vue({
             
             let angka1 = parseFloat(this.bill);
             let angka2 = parseFloat(this.bil2);
+            let hasil = 0;
+            let simbolOperasi = this.operasi;
+
+            if (simbolOperasi === '*') simbolOperasi = 'x'; // Mengubah tampilan * menjadi x di riwayat
 
             switch(this.operasi) {
                 case '+': 
-                    this.hasilKalkulasi = angka1 + angka2;
+                    hasil = angka1 + angka2;
                     break;
                 case '-': 
-                    this.hasilKalkulasi = angka1 - angka2;
+                    hasil = angka1 - angka2;
                     break;
                 case '*': 
-                    this.hasilKalkulasi = angka1 * angka2;
+                    hasil = angka1 * angka2;
                     break;
                 case '/': 
-                    // Validasi pembagian dengan angka 0
-                    this.hasilKalkulasi = angka2 !== 0 ? angka1 / angka2 : 'Tidak bisa dibagi 0';
+                    hasil = angka2 !== 0 ? angka1 / angka2 : 'Tidak bisa dibagi 0';
                     break;
                 default: 
-                    this.hasilKalkulasi = 0;
+                    hasil = 0;
             }
+
+            this.hasilKalkulasi = hasil;
+
+            // Masukkan data ke riwayat di urutan paling atas (paling baru)
+            this.riwayat.unshift({
+                id: Date.now(),
+                teks: `${angka1} ${simbolOperasi} ${angka2} = `,
+                total: hasil,
+                waktu: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+            });
         },
         resetKalkulator() {
             this.bill = '';
@@ -41,10 +54,17 @@ var app = new Vue({
             this.operasi = '+';
             this.hasilKalkulasi = 0;
             
-            // Menghilangkan kursor fokus dari input setelah reset dilakukan
             if (document.activeElement) {
                 document.activeElement.blur();
             }
+        },
+        hapusSatu(id) {
+            // Menghapus riwayat berdasarkan ID tertentu
+            this.riwayat = this.riwayat.filter(item => item.id !== id);
+        },
+        hapusSemua() {
+            // Mengosongkan seluruh isi array riwayat
+            this.riwayat = [];
         }
     }
 });
